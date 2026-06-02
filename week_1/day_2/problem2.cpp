a6748eb746eeee60f3e25efa289272582f94dbc1
anushkasahu20051111-lang <anushkasahu20051111@gmail.com>
2026-06-02 15:39:03 +0530

    Create problem2.cpp

---
+++ week_1/day_2/problem2.cpp
@@ -0,0 +1,58 @@
+class Codec {
+public:
+
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        if(!root) return "";
+        string s="";
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            TreeNode* curNode=q.front();
+            q.pop();
+            if(curNode==nullptr) s.append("#,");
+            else s.append(to_string(curNode->val)+',');
+            if(curNode!=nullptr){
+                q.push(curNode->left);
+                q.push(curNode->right);
+            }
+        }
+        return s;
+    } 
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+      if(data.size()==0) return nullptr;
+      stringstream s(data);
+      string str;
+      getline(s,str,',');
+      TreeNode* root=new TreeNode(stoi(str));
+      queue<TreeNode*> q;
+      q.push(root);
+      while(!q.empty()){
+        TreeNode* node=q.front();
+        q.pop();
+        getline(s,str,',');
+        if(str=="#"){
+            node->left=nullptr;
+        }
+        else{
+            TreeNode* leftNode=new TreeNode(stoi(str));
+            node->left=leftNode;
+            q.push(leftNode);
+        }
+        getline(s,str,',');
+        if(str=="#"){
+            node->right=nullptr;
+        }
+        else{
+            TreeNode* rightNode=new TreeNode(stoi(str));
+            node->right=rightNode;
+            q.push(rightNode);
+        }
+      }
+      return root; 
+    }
+};
+
+
