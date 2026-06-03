b83ccc6ecbecdbb1b0561a79c1eddc189f364c05
anushkasahu20051111-lang <anushkasahu20051111@gmail.com>
2026-06-03 11:15:35 +0530

    Create problem3.cpp

---
+++ week_1/day_3/problem3.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int cnt = 0;
+    int ans = 0;
+    void inorder(TreeNode* root, int k) {
+        if (root == nullptr) return;
+        inorder(root->left, k);
+        cnt++;
+        if (cnt == k) {
+            ans = root->val;
+            return;
+        }
+        inorder(root->right, k);
+    }
+    int kthSmallest(TreeNode* root, int k) {
+        inorder(root, k);
+        return ans;
+    }
+};
