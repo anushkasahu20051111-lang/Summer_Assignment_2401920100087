e569f91fa85e5baf116543a8a50bb93dd883a005
anushkasahu20051111-lang <anushkasahu20051111@gmail.com>
2026-06-05 20:41:12 +0530

    Create problem_1.cpp

---
+++ week_1/day_5/problem_1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    long long countGood(vector<int>& nums, int k) {
+        unordered_map<int, long long> freq; 
+        long long pairs = 0;
+        long long ans = 0;       
+        int left = 0;
+        for (int right = 0; right < nums.size(); right++) {
+            pairs += freq[nums[right]];
+            freq[nums[right]]++;
+            while (pairs >= k) {
+                ans += nums.size() - right;
+                freq[nums[left]]--;
+                pairs -= freq[nums[left]];
+                left++;
+            }
+        }
+        return ans;
+    }
+};
