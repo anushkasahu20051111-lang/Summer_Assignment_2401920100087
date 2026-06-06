a31eb07f28118c513d551c516b7090e891549c48
anushkasahu20051111-lang <anushkasahu20051111@gmail.com>
2026-06-06 15:04:38 +0530

    Create problem_3.cpp

---
+++ week_1/day_6/problem_3.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int l = 0, r = 0;
+        int maxFreq = 0;
+        int maxLen = 0;
+        vector<int> hash(26, 0);
+        while (r < s.size()) {
+            hash[s[r] - 'A']++;
+            maxFreq = max(maxFreq, hash[s[r] - 'A']);
+            while ((r - l + 1) - maxFreq > k) {
+                hash[s[l] - 'A']--;
+                l++;
+            }
+            maxLen = max(maxLen, r - l + 1);
+            r++;
+        }
+        return maxLen;
+    }
+};
