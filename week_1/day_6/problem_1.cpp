5eb8e630ff8d074965e14434ae489c187d6c8aa1
anushkasahu20051111-lang <anushkasahu20051111@gmail.com>
2026-06-06 15:02:56 +0530

    Create problem_1.cpp

---
+++ week_1/day_6/problem_1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int l = 0, r = 0;
+        int maxLen = 0;
+        unordered_map<int, int> mpp;
+        while (r < fruits.size()) {
+            mpp[fruits[r]]++;
+            while (mpp.size() > 2) {
+                mpp[fruits[l]]--;
+                if (mpp[fruits[l]] == 0) {
+                    mpp.erase(fruits[l]);
+                }
+                l++;
+            }
+            maxLen = max(maxLen, r - l + 1);
+            r++;
+        }
+        return maxLen;
+    }
+};
