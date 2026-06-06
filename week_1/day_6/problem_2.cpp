cad25ad63aeb8d26531cbe500dfb9cc6f667316e
anushkasahu20051111-lang <anushkasahu20051111@gmail.com>
2026-06-06 15:03:46 +0530

    Create problem_2.cpp

---
+++ week_1/day_6/problem_2.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        vector<int> last(3, -1);
+        int count = 0;
+        for (int i = 0; i < s.size(); i++) {
+            last[s[i] - 'a'] = i;
+            count += 1 + min({last[0], last[1], last[2]});
+        }
+        return count;
+    }
+};
