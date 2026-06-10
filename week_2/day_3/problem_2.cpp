fcf65f223040ed77e8ce4d318f2d2e46ef311d3d
anushkasahu20051111-lang <anushkasahu20051111@gmail.com>
2026-06-10 16:29:10 +0530

    Create problem_2.cpp

---
+++ week_2/day_3/problem_2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int mini = 0, maxi = 0;
+        for (char ch : s) {
+            if (ch == '(') {
+                mini++;
+                maxi++;
+            }
+            else if (ch == ')') {
+                mini--;
+                maxi--;
+            }
+            else {
+                mini--;
+                maxi++;
+            }
+            if (mini < 0) mini = 0;
+            if (maxi < 0) return false;
+        }
+        return mini == 0;
+    }
+};
