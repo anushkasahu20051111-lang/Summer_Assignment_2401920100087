627a0177008593dbcb3788c82a7991a6b022a281
anushkasahu20051111-lang <anushkasahu20051111@gmail.com>
2026-06-21 14:10:09 +0530

    Create problem_1.cpp

---
+++ week_3/day_7/problem_1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode* prev = dummy;
+        ListNode* curr = head;
+        while (curr) {
+            if (curr->next && curr->val == curr->next->val) {
+                int dup = curr->val;
+                while (curr && curr->val == dup) {
+                    curr = curr->next;
+                }
+                prev->next = curr;
+            }
+            else {
+                prev = curr;
+                curr = curr->next;
+            }
+        }
+        return dummy->next;
+    }
+};
+              
