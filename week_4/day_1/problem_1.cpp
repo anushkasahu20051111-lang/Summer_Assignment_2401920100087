889de2fec115e32364d29b20a0190560cecdf62d
anushkasahu20051111-lang <anushkasahu20051111@gmail.com>
2026-06-22 08:43:25 +0530

    Create problem_1.cpp

---
+++ week_4/day_1/problem_1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int totalgas=0;
+        int totalcost=0;
+        int tank=0;
+        int start=0;
+        for(int i=0;i<gas.size();i++){
+            totalgas+=gas[i];
+            totalcost+=cost[i];
+            tank=tank+gas[i]-cost[i];
+            if(tank<0){
+                start=i+1;
+                tank=0;
+            }
+        }
+        if(totalgas<totalcost){
+            return -1;
+        }
+        return start;
+    }
+};
